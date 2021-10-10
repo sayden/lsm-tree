ec651d47dd602b49a006b2c15f8e0f5b5ef17e9a
Mario Castro <mariocaster@gmail.com>
2021-10-10 19:13:15 +0200

    Add and find methods on wal

--- record.zig
+++ record.zig
@@ -1,69 +1,146 @@
 const std = @import("std");
 
-/// TODO Update this comment
+pub const RecordError = error{ BufferTooSmall, KeyTooBig };
+
 /// A record is an array of contiguous bytes in the following form:
-/// N bytes to store the total bytes that the record uses, where N = record_size_byte_length
-/// K bytes to store the key length where K = key_byte_size
+/// N bytes to store the total bytes that the record uses, where N = RecordLengthType (u64 by default)
+/// K bytes to store the key length where K = KeyLengthType (u32 by default which uses 4 bytes)
 /// K' bytes to store the key, where K' = key.len
 /// V bytes to store the value, where V = value.len
-fn Record() type {
+///
+/// For example the key:'hello' and value:'world' uses 22 bytes with u32 max size keys and u64 max size record
+/// 8 (u64) + 4 (u32) + 5 (bytes of 'hello') + 5 (bytes of 'world')
+pub fn Record(comptime KeyLengthType: type, comptime RecordLengthType: type) type {
     return struct {
         key: []u8,
         value: []u8,
 
+        var length: ?RecordLengthType = null;
+
         const Self = @This();
 
-        pub fn bytes(self: *const Self) !usize {
-            const key_size = std.mem.toBytes(self.key.len); // The size of the key serialized to 8 bytes
-            const value_size = std.mem.toBytes(self.value.len); //The size of the value serialized to 8 bytes
-            const kv_size = key_size.len + self.key.len + value_size.len + self.value.len;
-            try std.testing.expect(kv_size == 8 + 8 + 5 + 5);
-            const kv_size_bytes = std.mem.toBytes(kv_size);
-            const buf_size = kv_size_bytes.len + kv_size;
+        pub fn len(self: *const Self) RecordLengthType {
+            if (length)|l|{
+                return l;
+            }
 
-            return buf_size;
+            // N bytes to store a number that indicates how many bytes the record has
+            const record_length = @sizeOf(RecordLengthType);
 
-            // var total_size = record_size + key_byte_size + self.key.len + self.value.len;
-            // var buf: [total_size]u8 = undefined;
+            // K bytes to store a number that indicates how many bytes the key has
+            const key_length = @sizeOf(KeyLengthType);
 
-            // var offset: usize = 0;
+            // Total
+            length = record_length + key_length + self.key.len + self.value.len;
+            return length.?;
+        }
 
-            // //TODO check record size
-            // std.mem.copy(u8, buf[offset..record_size_byte_length], record_size[0..]);
-            // offset = record_size_byte_length;
+        /// Writes into the provided buf the data of the record in a contiguous array as described
+        /// in fn Record()
+        pub fn bytes(self: *const Self, buf: []u8) RecordError!void {
+            var offset: usize = 0;
 
-            // std.mem.copy(u8, buf[offset .. offset + key_byte_size], key_size[0..]);
-            // offset += key_size.len;
+            const record_size = self.len();
 
-            // std.mem.copy(u8, buf[offset .. offset + self.key.len], self.key);
-            // offset += self.key.len;
+            //Abord early if necessary
+            if (buf.len < record_size) {
+                return RecordError.BufferTooSmall;
+            }
 
-            // std.mem.copy(u8, buf[offset .. offset + self.value.len], self.value);
-            // offset += self.value.len;
+            if (self.key.len > std.math.maxInt(KeyLengthType)) {
+                return RecordError.KeyTooBig;
+            }
 
-            // const record_size: []u8 = std.mem.toBytes(total_size);
+            // No failures expected from here
 
-            // if (offset != total_size) {
-            //     std.debug.print("offset: {d}, total size: {d}\n", .{ offset, total_size });
-            //     unreachable;
-            // }
+            // Write N bytes to indicate the total size of the record. N is defined as the number of bytes
+            // that a type RecordLengthType can store (8 for u64, 4 for a u32, etc.)
+            std.mem.writeIntSliceLittle(RecordLengthType, buf[offset .. offset + @sizeOf(RecordLengthType)], record_size);
+            offset += @sizeOf(RecordLengthType);
 
-            // return buf[0..];
+            // We can truncate here because we have already checked that the size will fit above
+            const temp = @truncate(KeyLengthType, self.key.len);
+            std.mem.writeIntSliceLittle(KeyLengthType, buf[offset .. offset + @sizeOf(KeyLengthType)], temp);
+            offset += @sizeOf(KeyLengthType);
+
+            // TODO Write a function that "Reads as stream" (alas Read interface) instead of copying values
+            std.mem.copy(u8, buf[offset .. offset + self.key.len], self.key);
+            offset += self.key.len;
+
+            std.mem.copy(u8, buf[offset .. offset + self.value.len], self.value);
+        }
+
+        pub fn read_key(buf: []u8) []u8 {
+            // read the key length
+            const record_length_size = @sizeOf(RecordLengthType);
+            const bytes_for_key_length = buf[record_length_size..record_length_size+@sizeOf(KeyLengthType)];
+            const key_length = std.mem.readIntSliceLittle(KeyLengthType, bytes_for_key_length);
+
+            // read as many bytes as defined before to get the key
+            const key_start = record_length_size + bytes_for_key_length.len;
+            const key = buf[key_start .. key_start + key_length];
+
+            return key;
+        }
+
+        pub fn read_record(buf: []u8) Self {
+            //Read the record length bytes (4 or 8 usually) to get the total length of the record
+            const bytes_for_record_length = buf[0..@sizeOf(RecordLengthType)];
+            const record_length = std.mem.readIntSliceLittle(RecordLengthType, bytes_for_record_length);
+
+            // read the key length
+            const bytes_for_key_length = buf[bytes_for_record_length.len..bytes_for_record_length.len+@sizeOf(KeyLengthType)];
+            const key_length = std.mem.readIntSliceLittle(KeyLengthType, bytes_for_key_length);
+
+            // read as many bytes as defined before to get the key
+            const key_start = bytes_for_record_length.len + bytes_for_key_length.len;
+            const key = buf[key_start .. key_start + key_length];
+
+            // read as many bytes as left to get the value
+            const value_start = key_start + key_length;
+            const value_length = record_length - value_start;
+            const value = buf[value_start .. value_start + value_length];
+
+            return Self{
+                .key = key,
+                .value = value,
+            };
         }
     };
 }
 
-test "bytes" {
-    var key = std.base64;
-    std.debug.print("{d}\n", .{key});
-    var value = std.mem.toBytes("world");
+test "bytes returns a contiguous array with the record" {
+    var key = "hello".*;
+    var value = "world".*;
 
-    const r = Record(){
+    const r = Record(u32, u64){
         .key = key[0..],
         .value = value[0..],
     };
 
-    const res = try r.bytes();
-    std.debug.print("{d}\n", .{res});
-    try std.testing.expect(res == 8 + 8 + 5 + 5);
+    var page = std.heap.page_allocator;
+    var buf = try page.alloc(u8, r.len());
+    defer page.free(buf);
+    try r.bytes(buf);
+    try std.testing.expectStringEndsWith(buf, "helloworld");
+    try std.testing.expect(!std.mem.eql(u8, buf, "helloworld"));
+    try std.testing.expect(r.len() == 22);
+}
+
+test "having an slice, read a record starting at an offset" {
+    // var offset = 0;
+    var record_bytes = [_]u8{
+        22, 0, 0, 0, 0, 0, 0, 0, //22 bytes
+        5, 0, 0, 0, //5 bytes of key
+        104, 101, 108, 108, 111, //hello (the key)
+        119, 111, 114, 108, 100, //world (the value)
+    };
+
+    const RecordType = Record(u32, u64);
+    const r = RecordType.read_record(record_bytes[0..]);
+    try std.testing.expect(std.mem.eql(u8, r.key, "hello"));
+    try std.testing.expect(std.mem.eql(u8, r.value, "world"));
+
+    const key = RecordType.read_key(record_bytes[0..]);
+    try std.testing.expect(std.mem.eql(u8, key, "hello"));
 }
--- wal.zig
+++ wal.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
+const record = @import("./record.zig");
+const Record = record.Record;
+const RecordError = record.RecordError;
 
-fn Wal(comptime size: usize) type {
+const WalError = error{
+    MaxSizeReached,
+} || RecordError || std.mem.Allocator.Error;
+
+fn Wal(comptime size: usize, comptime RecordType: type) type {
     return struct {
         const Self = @This();
 
@@ -8,19 +15,23 @@ fn Wal(comptime size: usize) type {
         records: usize,
         maxsize: usize,
 
-        mem: []u8,
+        mem: [size]u8,
 
-        // Default values doesn't work here so I have to do an init function
-        pub fn init(self: *Self, allocator: *std.mem.Allocator) !*Self {
+        pub fn init(self: *Self, _: *std.mem.Allocator) !*Self {
             self.current_offset = 0;
             self.records = 0;
             self.maxsize = 1000 * size;
-            self.mem = try allocator.alloc(u8, size);
+            // self.mem = try allocator.alloc(u8, size);
 
             return self;
         }
 
-        pub fn add(self: *Self, bytes: []const u8) WalError!void {
+        pub fn add(self: *Self, bytes: []u8) WalError!void {
+            // Check if there's available space in the WAL
+            if (self.mem.len - self.current_offset < bytes.len) {
+                return WalError.MaxSizeReached;
+            }
+
             for (bytes) |b, i| {
                 self.mem[self.current_offset + i] = b;
             }
@@ -29,32 +40,140 @@ fn Wal(comptime size: usize) type {
             self.records += 1;
             self.current_offset += bytes.len;
         }
+
+        pub fn add_record(self: *Self, r: *const RecordType, alloc: *std.mem.Allocator) WalError!void {
+            const record_length = r.len();
+
+            // Check if there's available space in the WAL
+            if (self.mem.len - self.current_offset < record_length) {
+                return WalError.MaxSizeReached;
+            }
+
+            var buf: []u8 = try alloc.alloc(u8, record_length);
+            defer alloc.free(buf);
+            try r.bytes(buf);
+
+            return self.add(buf);
+        }
+
+        // TODO Find returns first ocurrence when it should be returning last ocurrence found which
+        // is the most recent
+        pub fn find(self: *Self, key_to_find: []u8) WalError!?RecordType {
+            var offset: usize = 0;
+            var r: RecordType = undefined;
+
+            while (offset < self.current_offset) {
+                r = RecordType.read_record(self.mem[offset..]);
+                if (std.mem.eql(u8, r.key, key_to_find)) {
+                    return r;
+                }
+                offset += r.len();
+            }
+
+            return null;
+        }
     };
 }
 
-const WalError = error{
-    MaxSizeReached,
-};
-
-test "test add" {
-    var page = std.heap.page_allocator;
-    var arena = std.heap.ArenaAllocator.init(page);
+test "add bytes" {
+    var alloc = std.testing.allocator;
+    var arena = std.heap.ArenaAllocator.init(alloc);
     defer arena.deinit();
 
+    var hello = "hello".*;
+    var world = "world".*;
+
     const allocator = &arena.allocator;
-    var temp = try allocator.create(Wal(100));
+    var temp = try allocator.create(Wal(100, u8));
 
     var wal = try temp.init(allocator);
-    try wal.add("hello");
+    try wal.add(hello[0..]);
 
     try std.testing.expect(wal.records == 1);
-    try std.testing.expect(wal.current_offset == "hello".len);
+    try std.testing.expect(wal.current_offset == hello.len);
     try std.testing.expect(wal.mem.len == 100);
-    try std.testing.expect(std.mem.eql(u8, wal.mem[0..wal.current_offset], "hello"));
+    try std.testing.expect(std.mem.eql(u8, wal.mem[0..wal.current_offset], hello[0..]));
 
-    try wal.add("world");
+    try wal.add(world[0..]);
     try std.testing.expect(wal.records == 2);
-    try std.testing.expect(wal.current_offset == "hello".len + "world".len);
+    try std.testing.expect(wal.current_offset == hello.len + world.len);
     try std.testing.expect(wal.mem.len == 100);
     try std.testing.expect(std.mem.eql(u8, wal.mem[0..wal.current_offset], "helloworld"));
+}
+
+test "add record" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = &arena.allocator;
+
+    var temp = try alloc.create(Wal(std.mem.page_size * 1000, Record(u32, u64)));
+    var wal = try temp.init(alloc);
+
+    var key = "hello".*;
+    var value = "world".*;
+    const r = Record(u32, u64){
+        .key = key[0..],
+        .value = value[0..],
+    };
+    try wal.add_record(&r, alloc);
+
+    try std.testing.expect(wal.records == 1);
+    try std.testing.expect(wal.current_offset == r.len());
+    try std.testing.expectStringEndsWith(wal.mem[0..22], "helloworld");
+}
+
+test "max size reached" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = &arena.allocator;
+
+    var temp = try alloc.create(Wal(10, Record(u32, u64)));
+    var wal = try temp.init(alloc);
+
+    var key = "hello".*;
+    var value = "world".*;
+    const r = Record(u32, u64){
+        .key = key[0..],
+        .value = value[0..],
+    };
+
+    if (wal.add_record(&r, alloc)) |_| unreachable else |err| {
+        try std.testing.expect(err == WalError.MaxSizeReached);
+    }
+
+    var buf: [22]u8 = undefined;
+    try r.bytes(buf[0..]);
+
+    if (wal.add(buf[0..])) |_| unreachable else |err| {
+        try std.testing.expect(err == WalError.MaxSizeReached);
+    }
+}
+
+test "find a key" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = &arena.allocator;
+
+    var temp = try alloc.create(Wal(100, Record(u32, u64)));
+    var wal = try temp.init(alloc);
+
+    var key = "hello".*;
+    var value = "world".*;
+    const r = Record(u32, u64){
+        .key = key[0..],
+        .value = value[0..],
+    };
+    try wal.add_record(&r, alloc);
+
+    try std.testing.expect(wal.records == 1);
+    try std.testing.expect(wal.current_offset == r.len());
+    try std.testing.expectStringEndsWith(wal.mem[0..22], "helloworld");
+
+    const maybe_record = try wal.find(key[0..]);
+    try std.testing.expect(std.mem.eql(u8, maybe_record.?.value, value[0..]));
+
+    var unknown_key = "unknokwn".*;
+
+    const unkonwn_record = try wal.find(unknown_key[0..]);
+    try std.testing.expect(unkonwn_record == null);
 }
