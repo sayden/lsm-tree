faf0638c9373bb504fe78d0a47bbc09129b824cb
Mario Castro <mariocaster@gmail.com>
2021-10-17 23:32:42 +0200

    header definition

--- header.zig
+++ header.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
-const Header = struct {
+pub const Header = struct {
+    magic_number: u32 = 0,
     pointer_byte_offset: usize,
-    first_key: []u8,
-    last_key: []u8,
+    first_key_offset: usize,
+    last_key_offset: usize,
+
+    pub fn header_size(self: *const Header) usize {
+        return @sizeOf(@TypeOf(self.magic_number))
+        + (@sizeOf(usize)*3);
+    }
 };
+
+pub fn toBytes(h: *const Header, buf: []u8) void {
+    const magic_number_size = @sizeOf(@TypeOf(h.magic_number));
+    
+    std.mem.writeIntLittle(@TypeOf(h.magic_number), buf[0..magic_number_size], h.magic_number);
+    std.mem.writeIntLittle(usize, buf[magic_number_size..magic_number_size+8], h.first_key_offset);
+    std.mem.writeIntLittle(usize, buf[magic_number_size+8..magic_number_size+16], h.last_key_offset);
+    std.mem.writeIntLittle(usize, buf[magic_number_size+16..magic_number_size+24], h.pointer_byte_offset);
+}
