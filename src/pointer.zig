c9cdbe7ebace295ceb1d001a3f562fe91a206a8a
Mario Castro <mariocaster@gmail.com>
2022-02-19 16:48:39 +0100

    Fix and comment some pointer stuff

--- src/pointer.zig
+++ src/pointer.zig
@@ -2,15 +2,31 @@ const std = @import("std");
 const KeyLengthType = @import("./record.zig").KeyLengthType;
 const Op = @import("ops.zig").Op;
 
+// A pointer contains an Operation, a key and a offset to find the Value of the record.
+// The pointer is stored as follows:
+// 1 byte: Operation
+// 2 bytes: Key size
+// X bytes: Key
+// 8 bytes: Offset in the data
 pub const Pointer = struct {
+    op: Op,
     key: []const u8,
     byte_offset: usize = 0,
-    op: Op,
 
-    pub const key_size: usize = @sizeOf(KeyLengthType);
     const Self = @This();
 
     pub fn bytesLen(self: *const Self) usize {
-        return 1 + Self.key_size + self.key.len + @sizeOf(@TypeOf(self.byte_offset));
+        return 1 + @sizeOf(KeyLengthType) + self.key.len + @sizeOf(@TypeOf(self.byte_offset));
     }
 };
+
+test "pointer.bytesLen" {
+    var p = Pointer{
+        .op = Op.Create,
+        .key = "hello",
+        .byte_offset = 100,
+    };
+
+    var len = p.bytesLen();
+    try std.testing.expectEqual(@as(usize, 16), len);
+}
--- src/serialize/pointer.zig
+++ src/serialize/pointer.zig
@@ -5,6 +5,8 @@ const KeyLengthType = @import("lsmtree").KeyLengthType;
 const Pointer = @import("lsmtree").Pointer;
 const Op = @import("lsmtree").Op;
 
+const Error = error{BufferTooSmall};
+
 pub fn fromRecord(r: Record, buf: []u8, file_offset: usize) usize {
     // op
     buf[0] = @enumToInt(r.op);
@@ -25,44 +27,62 @@ pub fn fromRecord(r: Record, buf: []u8, file_offset: usize) usize {
     return offset;
 }
 
-pub fn toBytesAlloc(self: Pointer, file_offset: usize, allocator: *std.mem.Allocator) ![]u8 {
-    const returned_bytes_length = @sizeOf(KeyLengthType) + self.key.len + @sizeOf(usize);
-    var buf = try allocator.alloc(u8, returned_bytes_length);
-    _ = self.bytes(&buf, file_offset);
+// Get a byte array representation of a pointer using the provided allocator.
+// FREE the returned array using the provided allocator. The array look like this:
+// 1 byte: Operation
+// 2 bytes: Key size
+// X bytes: Key
+// 8 bytes: Offset in the data
+pub fn toBytesAlloc(self: Pointer, allocator: *std.mem.Allocator) ![]u8 {
+    var buf = try allocator.alloc(u8, self.bytesLen());
+    _ = toBytes(self, buf) catch |err|
+        return err;
+
     return buf;
 }
 
-pub fn toBytes(self: Pointer, buf: []u8) usize {
+// Get a byte array representation of a pointer using the provided array.
+// It must be at least of the size of Pointer.bytesLen(). The array look like this:
+// 1 byte: Operation
+// 2 bytes: Key size
+// X bytes: Key
+// 8 bytes: Offset in the data
+pub fn toBytes(self: Pointer, buf: []u8) Error!usize {
+    if (self.bytesLen() < buf.len) {
+        return Error.BufferTooSmall;
+    }
+
     var offset: usize = 0;
 
     // Op
-    std.mem.writeIntSliceLittle(@TypeOf(self.op), buf[0]);
+    buf[0] = @enumToInt(self.op);
+    // std.mem.writeIntSliceLittle(u8, buf[0], @enumToInt(self.op));
     offset += 1;
 
     // key length
-    std.mem.writeIntSliceLittle(KeyLengthType, buf[offset .. offset + Pointer.key_size], self.key.len);
-    offset += Pointer.key_size;
+    std.mem.writeIntSliceLittle(KeyLengthType, buf[offset .. offset + @sizeOf(KeyLengthType)], @truncate(KeyLengthType, self.key.len));
+    offset += @sizeOf(KeyLengthType);
 
     // key
     std.mem.copy(u8, buf[offset .. offset + self.key.len], self.key);
     offset += self.key.len;
 
     //offset
-    std.mem.writeIntSliceLittle(usize, buf[offset .. offset + Pointer.key_size], offset);
-    offset += @sizeOf(usize);
+    std.mem.writeIntSliceLittle(usize, buf[offset .. offset + @sizeOf(@TypeOf(self.byte_offset))], self.byte_offset);
+    offset += @sizeOf(@TypeOf(self.byte_offset));
 
     return offset;
 }
 
 pub fn fromBytes(bytes: []u8) Pointer {
     //Op
-    std.debug.print("data: '{d}'\n", .{bytes});
+    // std.debug.print("data: '{d}'\n", .{bytes});
     var op = @intToEnum(Op, bytes[0]);
     var offset: usize = 1;
 
     //Key length
-    var key_length = std.mem.readIntSliceLittle(KeyLengthType, bytes[offset .. offset + Pointer.key_size]);
-    offset += Pointer.key_size;
+    var key_length = std.mem.readIntSliceLittle(KeyLengthType, bytes[offset .. offset + @sizeOf(KeyLengthType)]);
+    offset += @sizeOf(KeyLengthType);
 
     // Key
     var key = bytes[offset .. offset + key_length];
@@ -71,7 +91,7 @@ pub fn fromBytes(bytes: []u8) Pointer {
     // Offset
     var byte_offset = std.mem.readIntSliceLittle(usize, bytes[offset .. offset + @sizeOf(usize)]);
 
-    std.debug.print("op: '{d}', key length: {d}, key: '{s}', offset: {d}\n", .{ bytes[0], key_length, key, offset });
+    // std.debug.print("op: '{d}', key length: {d}, key: '{s}', offset: {d}\n", .{ bytes[0], key_length, key, offset });
 
     return Pointer{
         .key = key,
@@ -80,21 +100,46 @@ pub fn fromBytes(bytes: []u8) Pointer {
     };
 }
 
-test "pointer.read" {
-    var buf: [16]u8 = undefined;
-    buf[0] = 0;
-    std.mem.writeIntSliceLittle(u16, buf[1..3], 5);
-    std.mem.copy(u8, buf[3..8], "hello");
-    std.mem.writeIntSliceLittle(usize, buf[8..16], 99);
+test "pointer.fromBytes" {
+    var buf = [_]u8{
+        0, //Op
+        5, 0, //key length
+        104, 101, 108, 108, 111, //hello (the key)
+        100, 0, 0, 0, 0, 0, 0, 0, //offset
+    };
 
     const p = fromBytes(&buf);
     const eq = std.testing.expectEqual;
     try eq(@as(usize, 5), p.key.len);
-    try eq(@as(usize, 99), p.byte_offset);
+    try eq(@as(usize, 100), p.byte_offset);
 
     try std.testing.expectEqualSlices(u8, "hello", p.key);
 
     try eq(@as(usize, 16), p.bytesLen());
+}
+
+test "pointer.toBytes" {
+    var buf = [_]u8{
+        0, //Op
+        5, 0, //key length
+        104, 101, 108, 108, 111, //hello (the key)
+        100, 0, 0, 0, 0, 0, 0, 0, //offset
+    };
+
+    var p = Pointer{
+        .op = Op.Create,
+        .key = "hello",
+        .byte_offset = 100,
+    };
+
+    var alloc = std.testing.allocator;
+    var res = try toBytesAlloc(p, &alloc);
+    defer alloc.free(res);
+
+    for (buf) |b, i| {
+        // std.debug.print("{d} vs {d}\n", .{ res[i], b });
+        try std.testing.expectEqual(res[i], b);
+    }
 }
 
 test "pointer.fromRecord" {
